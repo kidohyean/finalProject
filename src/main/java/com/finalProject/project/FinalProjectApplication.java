689c89a0e5f45e93f4b952ffb5397dfde9d4b53e
Sareukil <wjstk5128@gmail.com>
2023-01-05 20:40:07 +0900

    0105_1-mappers,config,security

--- src/main/java/com/finalProject/project/FinalProjectApplication.java
+++ src/main/java/com/finalProject/project/FinalProjectApplication.java
@@ -1,11 +1,13 @@
 package com.finalProject.project;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.finalProject.project"})
+@MapperScan(basePackages = {"com.finalProject.project"})
 public class FinalProjectApplication {
 
 	public static void main(String[] args) {
