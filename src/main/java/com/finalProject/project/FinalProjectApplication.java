40806dde50db1704f6661c90ca8cef4cf90289b0
Sareukil <wjstk5128@gmail.com>
2023-01-05 18:50:28 +0900

    시작

---
+++ src/main/java/com/finalProject/project/FinalProjectApplication.java
@@ -0,0 +1,15 @@
+package com.finalProject.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.finalProject.project"})
+public class FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>index 페이지</title>
+	</head>
+	<body>
+		<h3>test</h3>
+		
+	</body>
+</html>
