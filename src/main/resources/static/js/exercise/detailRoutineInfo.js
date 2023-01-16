1224411d4ee517b7f693a862545e4ce2c2f8dcce
kang1597 <35wkd2817@gmail.com>
2023-01-16 17:50:10 +0900

    운동루틴상세정보 수정

--- src/main/resources/static/js/exercise/detailRoutineInfo.js
+++ src/main/resources/static/js/exercise/detailRoutineInfo.js
@@ -1,1 +1,11 @@
+$(document).ready(function(){
+ $('.flex1 a').click(function(){
+            let index = $(this).index();
+            index++;
+            $('.flex1 a').removeClass('active');
+            $(this).addClass('active');
 
+            $('.content1-1').removeClass('on');
+            $('#contents'+index).addClass('on');
+        })
+});
